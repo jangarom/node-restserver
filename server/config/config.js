cb643a02195b6755087cd93730c2adc775c1462c
Jangarom <jangarom91@gmail.com>
2019-12-02 20:34:42 +0100

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://jangarom:1g36zvbdzXrwLhn2@jangaromcluster-lwngl.mongodb.net/cafe?retryWrites=true&w=majority';
+    // urlDB = 'mongodb+srv://jangarom:1g36zvbdzXrwLhn2@jangaromcluster-lwngl.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
